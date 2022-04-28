00ca11b3683ee426e3d4f32ea43a13cb68116d9c
Oxana2302 <roxyss84@mail.ru>
2022-04-28 13:20:54 +0300

    task2_1done

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,13 +7,16 @@ function calcShipping(sum, min, shipping) {
 
     // создайте переменную shippingSum
 
-    // если productsSum равно 0,
+    if(productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = 0
+    } // если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
+    if(productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice}// если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
